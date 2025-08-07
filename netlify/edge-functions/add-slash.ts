7b16ff3feedb0f82f3bdda794554698815ee942a
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-07 22:19:42 +0800

    change logs

--- netlify/edge-functions/add-slash.ts
+++ netlify/edge-functions/add-slash.ts
@@ -1,9 +1,9 @@
 import type { Config, Context } from '@netlify/edge-functions'
 
 export default async function handler(request: Request, context: Context) {
-  console.log('context.url:', context.url)
   const originalUrl = context.url.searchParams.get('x-middleware-original-url')
   const rewrite = request.headers.get('x-middleware-rewrite')
+  console.log('originalUrl:', originalUrl, 'rewrite:', rewrite)
   if (originalUrl && rewrite) {
     return Response.redirect(`${originalUrl}?/x-redirected=true`)
   }
