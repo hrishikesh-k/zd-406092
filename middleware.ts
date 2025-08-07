d0d34a8f350156a41e937372e472aa564bb9a214
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-07 21:42:18 +0800

    try adding next

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,13 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl)
+  const headers = new Headers()
+  headers.set('x-middleware-original-url', request.url)
+  headers.set('x-middleware-rewrite-2', new URL('/page-1/', request.url).toString())
+  const res = NextResponse.next({
+    headers
+  })
+  console.log(res)
   return NextResponse.rewrite(new URL('/page-1/', request.url))
 }
 
--- netlify/edge-functions/add-slash.ts
+++ netlify/edge-functions/add-slash.ts
@@ -1,9 +1,9 @@
 import type { Config, Context } from '@netlify/edge-functions'
 
-export default async function handler(request: Request, context: Context) {
-  const rewrite = request.headers.get('x-middleware-rewrite')
+export default async function handler(request: Request) {
+  const rewrite = request.headers.get('x-middleware-rewrite-2')
   if (rewrite) {
-    return Response.redirect(rewrite)
+    return Response.redirect(request.headers.get('x-middleware-original-url') || rewrite)
   }
 }
 
