0ef13696daccd9283e0d1529b2f9018e47d46c5d
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-07 21:35:51 +0800

    log nextUrl

--- netlify/edge-functions/add-slash.ts
+++ netlify/edge-functions/add-slash.ts
@@ -1,6 +1,6 @@
-import type { Config } from '@netlify/edge-functions'
+import type { Config, Context } from '@netlify/edge-functions'
 
-export default async function handler(request: Request) {
+export default async function handler(request: Request, context: Context) {
   const rewrite = request.headers.get('x-middleware-rewrite')
   if (rewrite) {
     return Response.redirect(rewrite)
