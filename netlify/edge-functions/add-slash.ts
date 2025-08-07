bf53f152bfe36bcb25fb4292953d91974a389e8c
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-07 21:16:37 +0800

    add logs for response

--- netlify/edge-functions/add-slash.ts
+++ netlify/edge-functions/add-slash.ts
@@ -1,6 +1,38 @@
-import type { Config } from '@netlify/edge-functions'
+import type { Config, Context } from '@netlify/edge-functions'
 
-export { addSlash as default } from 'https://esm.sh/gh/ascorbic/slash-edge/mod.ts'
+export default async function handler(request: Request, context: Context) {
+  if (context.url.pathname === '/' || request.headers.get('x-nf-sub-request')) {
+    return
+  }
+
+  for (const suffix of ['/index.html', '.html']) {
+    if (context.url.pathname.endsWith(suffix)) {
+      return Response.redirect(`${context.url.origin}${context.url.pathname.slice(
+        0,
+        -suffix.length
+      )}/${context.url.search}`, 301)
+    }
+  }
+
+  if (!context.url.pathname.endsWith('/') && !context.url.pathname.split('/').pop()?.includes('.')) {
+    return Response.redirect(`${context.url.origin}${context.url.pathname}/${context.url.search}`, 301)
+  }
+
+  const response = await context.next({
+    sendConditionalRequest: true
+  })
+
+  console.log(response)
+
+  if (response.status === 301 && !context.url.pathname.endsWith('/')) {
+    request.headers.set('x-nf-sub-request', '1')
+    const newLocation = new URL(response.headers.get('location') || '', request.url)
+    newLocation.search = context.url.search
+    return newLocation
+  }
+
+  return response
+}
 
 export const config: Config = {
   path: '/*'
