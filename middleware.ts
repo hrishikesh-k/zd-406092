597e04849863b34d5bd7cf9ad590db40fc7d48d0
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-08 18:07:58 +0800

    try context.next

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  console.log('nextUrl:', request.nextUrl)
   const rewriteUrl = new URL('/page-1/', request.url)
-  if (request.nextUrl.searchParams.has('x-redirected')) {
-    rewriteUrl.searchParams.set('x-rewrite-url', request.url)
-  } else {
+  if (!request.nextUrl.searchParams.has('x-redirected')) {
     rewriteUrl.searchParams.set('x-redirect-url', request.url)
   }
   return NextResponse.rewrite(rewriteUrl)
--- netlify/edge-functions/add-slash.ts
+++ netlify/edge-functions/add-slash.ts
@@ -4,12 +4,17 @@ export default async function handler(_: Request, context: Context) {
   const redirectUrl = context.url.searchParams.get('x-redirect-url')
   const rewriteUrl = context.url.searchParams.get('x-rewrite-url')
 
+  console.log('Redirect URL:', redirectUrl)
+  console.log('Rewrite URL:', rewriteUrl)
+
   if (redirectUrl) {
     return Response.redirect(`${redirectUrl}?x-redirected=true`)
   }
 
   if (rewriteUrl) {
-    return new URL(rewriteUrl || '')
+    return await context.next(
+      new Request(rewriteUrl)
+    )
   }
 }
 
