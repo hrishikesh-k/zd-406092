a944d99ebf56a50f5b6df20ea5f3c1285f13936d
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-08-08 18:12:10 +0800

    log nexturl

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
+  console.log('Request URL:', request.nextUrl)
   const rewriteUrl = new URL('/page-1/', request.url)
   if (!request.nextUrl.searchParams.has('x-redirected')) {
     rewriteUrl.searchParams.set('x-redirect-url', request.url)
